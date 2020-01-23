20dd148bc282323b6a9119dc62a644585633af30
Michal Šmeidler <smeidler.michal@gmail.com>
2020-01-23 18:19:00 +0100

    Use package vue-chartjs

--- src/components/TestChart.vue
+++ src/components/TestChart.vue
@@ -1,60 +1,27 @@
-<template>
-	<div class="chart">
-		<div class="row">
-			<canvas id="test-chart"></canvas>
-		</div>
-	</div>
-</template>
-
 <script>
-	import Chart from 'chart.js';
+	import {Line} from 'vue-chartjs'
 
 	export default {
 		name:    'TestChart',
-		data () {
-			return {
-				chart: null
-			}
-		},
-		methods: {},
-		mounted () {
-			const ctx = document.getElementById('test-chart');
-			this.chart = new Chart(ctx, {
-				type:    'bar',
-				data:    {
-					labels:   ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-					datasets: [{
-						label:           '# of Votes',
-						data:            [12, 19, 3, 5, 2, 3],
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)'
-						],
-						borderColor:     [
-							'rgba(255, 99, 132, 1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)'
-						],
-						borderWidth:     1
-					}]
-				},
-				options: {
-					scales: {
-						yAxes: [{
-							ticks: {
-								beginAtZero: true
-							}
-						}]
+		extends: Line,
+		data:    () => ({
+			chartdata: {
+				labels:   ['January', 'February'],
+				datasets: [
+					{
+						label:           'Data One',
+						backgroundColor: '#f87979',
+						data:            [40, 20]
 					}
-				}
-			});
+				]
+			},
+			options:   {
+				responsive:          true,
+				maintainAspectRatio: false
+			}
+		}),
+		mounted () {
+			this.renderChart(this.chartdata, this.options)
 		}
 	}
 </script>
