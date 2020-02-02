fafe66595906105109b95fb6dfc2355e0b4bd25d
Michal Šmeidler <smeidler.michal@gmail.com>
2020-02-02 21:46:09 +0100

    Implement commodity average data

--- src/components/CommodityPage.vue
+++ src/components/CommodityPage.vue
@@ -2,8 +2,95 @@
 	<div class="commodity-page">
 
 		<div class="d-sm-flex align-items-center justify-content-between mb-4">
-			<h1 class="h3 mb-0 text-gray-800">{{commodity}}</h1>
-			<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+			<h1 class="h3 mb-0 text-gray-800">{{this.getName()}}</h1>
+			<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+					class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+		</div>
+
+		<div class="row">
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-primary shadow h-100 py-2">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
+								<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+									Průměrná denní spotřeba
+								</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">{{average_day}} <span
+										v-html="this.getUnit()"></span></div>
+							</div>
+							<div class="col-auto">
+								<i class="fas fa-calendar fa-2x text-gray-300"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-success shadow h-100 py-2">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
+								<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+									Průměrná měsíční spotřeba
+								</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">{{average_month}} <span
+										v-html="this.getUnit()"></span></div>
+							</div>
+							<div class="col-auto">
+								<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Earnings (Monthly) Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-info shadow h-100 py-2">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
+								<div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+									Průměrná roční spotřeba
+								</div>
+								<div class="row no-gutters align-items-center">
+									<div class="col-auto">
+										<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{average_year}} <span
+												v-html="this.getUnit()"></span></div>
+									</div>
+								</div>
+							</div>
+							<div class="col-auto">
+								<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-warning shadow h-100 py-2">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
+								<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+									Měsíční záloha
+								</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">3000 Kč</div>
+							</div>
+							<div class="col-auto">
+								<i class="fas fa-comments fa-2x text-gray-300"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="row">
@@ -89,9 +176,17 @@
 	import CommodityChartV2 from './CommodityChartV2'
 	import MeterStates from './MeterStates'
 	import AddMeterState from './AddMeterState'
+	import ApiWrapper from '../model/ApiWrapper'
 
 	export default {
 		name:       'CommodityPage',
+		data () {
+			return {
+				average_day:   null,
+				average_month: null,
+				average_year:  null,
+			}
+		},
 		props:      {
 			commodity: String
 		},
@@ -99,6 +194,21 @@
 			CommodityChartV2,
 			MeterStates,
 			AddMeterState
+		},
+		methods:    {
+			getName () {
+				return this.commodity === 'gas' ? 'Plyn' : 'Elektřina';
+			},
+			getUnit () {
+				return this.commodity === 'gas' ? 'm<sup>3</sup>' : 'kW/h';
+			}
+		},
+		mounted () {
+			ApiWrapper.getAverageData(this.commodity).then((data) => {
+				this.average_day = Math.round(data.data[0].day_consumption)
+				this.average_month = Math.round(data.data[0].month_consumption)
+				this.average_year = Math.round(data.data[0].year_consumption)
+			})
 		}
 	}
 </script>
